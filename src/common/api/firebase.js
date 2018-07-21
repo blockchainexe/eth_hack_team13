724964b1b3450e3e69fa8d65d1d588e80380f24a
TakechiMami <takechimami@gmail.com>
2018-07-21 23:32:53 +0900

    add firebase functions

--- src/common/api/firebase.js
+++ src/common/api/firebase.js
@@ -17,3 +17,28 @@ export const initDB = () => {
 export const dbWrite = (path, data) => {
   firebase.database().ref(path).set(data);
 };
+
+export const dbRead = (path) => {
+  firebase.database().ref(path).on('value', function(snapshot) {
+    console.log(snapshot.val());
+  });
+};
+
+export const dbReadOnce = (path) => {
+  firebase.database().ref(path).once('value').then(function(snapshot) {
+    console.log(snapshot.val());
+  });
+};
+
+export const writeNewMessage = (id, username, message) => {
+  var postData = {
+    username: username,
+    message: message,
+  };
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('messages').push().key;
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['test/id/'+id+'/messages/' + newPostKey] = postData;
+  return firebase.database().ref().update(updates);
+}
