afbae213bf5251e6f37d82db3eefcc75f11961db
新谷大樹 <shintanidaiki@shintanidaikinoMacBook-ea.local>
2018-07-21 11:56:44 +0900

    友達リスト

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,4 +1,6 @@
 import Landing from '~/pages/Landing';
+import FriendsList from '~/pages/FriendsList';
 export const routes = [
-  { path: '/', component: Landing }
+  { path: '/', component: Landing },
+  { path: '/friends-list', component: FriendsList }
 ];
