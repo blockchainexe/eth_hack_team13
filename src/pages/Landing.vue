1f5acc17cb970a7513e4df22edcdcefe83490298
TakechiMami <takechimami@gmail.com>
2018-07-21 12:04:07 +0900

    fix conflict

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -7,6 +7,7 @@
       <div slot="body">detail</div>
       <div slot="footer"></div>
     </modal-basic>
+    <button @click="goToAvatar">go to avatar</button>
   </div>
 </template>
 
@@ -15,11 +16,19 @@ import ModalBasic from '~/components/ModalBasic'
 export default {
   data () {
     return {
-      showModal: false
+      showModal: false,
+      imgSrc: "http://hanasaka-kidan.com/wp-content/uploads/2016/11/35f2dca61c1d9c731d0c2ed665c2e3e3.png",
+      userName: "User Name",
+      itemList: [{itemImg: "item1", name: "name1"}, {itemImg: "item2", name: "name2"}]
     };
   },
   components: {
     ModalBasic
+  },
+  methods: {
+    goToAvatar: function(){
+      this.$router.push({ path: '/avatar', query: { imgSrc: this.imgSrc, userName: this.userName, itemList: this.itemList }})
+    }
   }
 };
 </script>
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,8 @@
 import Landing from '~/pages/Landing';
+import Avatar from '~/pages/Avatar';
 import FriendsList from '~/pages/FriendsList';
 export const routes = [
   { path: '/', component: Landing },
-  { path: '/friends-list', component: FriendsList }
+  { path: '/friends-list', component: FriendsList },
+  { path: '/avatar', component: Avatar }
 ];
