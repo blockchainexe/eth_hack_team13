733b3b39a7f045e01e3c8b4d4f3043e225f8cf59
Riku Murai <riku.murai15@imperial.ac.uk>
2018-07-21 18:20:03 +0900

    HelloLandingとかを消す

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,13 +1,10 @@
 <template>
   <div>
-    hello landing
-    <button id="show-modal" @click="showModal = true">Show Modal</button>
     <modal-basic v-if="showModal" @close="showModal = false">
       <h3 slot="header">friend name</h3>
       <div slot="body">detail</div>
       <div slot="footer"/>
     </modal-basic>
-    <button @click="goToAvatar">go to avatar</button>
   </div>
 </template>
 
